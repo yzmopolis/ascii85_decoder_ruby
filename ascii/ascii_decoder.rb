79976fc6a7c3db88486131056752b9e01d20af4a
ika.grabon <ika.grabon@gmail.com>
2018-01-03 17:27:13 +0100

    Add empty methods

--- ascii/ascii_decoder.rb
+++ ascii/ascii_decoder.rb
@@ -1,10 +1,11 @@
 module Ascii
-  def Ascii.decode
+  def Ascii.decode_from_ascii(code)
     puts "decode"
   end
-  def Ascii.code
+  def Ascii.encode_to_ascii85(str)
     puts "code"
   end
 end
 
-Ascii.code
+Ascii.encode_to_ascii85("ika")
+Ascii.decode_from_ascii("<~Bkq,~>")
