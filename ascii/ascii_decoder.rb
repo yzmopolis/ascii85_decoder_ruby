cf48ecc80aada8c11c1542bb137d5085964d5cab
ika.grabon <ika.grabon@gmail.com>
2018-01-03 16:52:06 +0100

    Add ascii

---
+++ ascii/ascii_decoder.rb
@@ -0,0 +1,10 @@
+module Ascii
+  def Ascii.decode
+    puts "decode"
+  end
+  def Ascii.code
+    puts "code"
+  end
+end
+
+Ascii.code
---
+++ ascii/my_test.rb
@@ -0,0 +1,10 @@
+gem 'minitest', '~> 5.4'
+require 'minitest/autorun'
+require_relative '../ascii/ascii_decoder'
+
+
+class AsciiTest < Minitest::Test
+  def test_the_first
+    puts "nothing"
+  end
+end
