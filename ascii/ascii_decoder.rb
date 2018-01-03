19b556e2a5a4a52026182f73c3e007d99b0110ff
ika.grabon <ika.grabon@gmail.com>
2018-01-03 19:45:13 +0100

    Add method decode_from_ascii85

--- ascii/ascii_decoder.rb
+++ ascii/ascii_decoder.rb
@@ -2,8 +2,19 @@ module Ascii
   MAGIC_NUMBER = 33
   MAGIC_DIVISOR = 85
 
-  def self.decode_from_ascii(block)
-    "not ready"
+  def self.decode_from_ascii85(code)
+    asc = code[2..-3].gsub('z', '!!!!!').delete(' ').delete("\n").delete("\t")
+    my_bits = (5 - asc.size % 5) % 5
+    my_nums = (asc + 'u' * my_bits).chars.map { |char| char.ord - MAGIC_NUMBER }
+    str = ''
+    until my_nums.empty?
+      a = my_nums.shift(5)
+      b = a.map.with_index { |val, i| val * MAGIC_DIVISOR**(4 - i) }.inject(:+).to_s(2).rjust(32, '0').chars
+      4.times do
+        str += b.shift(8).join.to_i(2).chr
+      end
+    end
+    str[0..-1 - my_bits]
   end
 
   def self.encode_to_ascii85(string)
@@ -24,5 +35,6 @@ module Ascii
 end
 
 
-# puts Ascii.decode_from_ascii("<~Bkq,~>")
+puts Ascii.decode_from_ascii85('<~Bkq,~>')
+puts Ascii.decode_from_ascii85("<~6tL4RB5_g'Df,~>")
 puts Ascii.encode_to_ascii85('bitchin')
